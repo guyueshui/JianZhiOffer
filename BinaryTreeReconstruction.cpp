2af3a86762584ed666d97551237be03cc7837f1f
guyueshui <guyueshui002@gmail.com>
2019-08-26 23:20:55 +0800

    在gdb的帮助下，终于调通了这道题，思路很早就有了，就是写不对！

--- BinaryTreeReconstruction.cpp
+++ BinaryTreeReconstruction.cpp
@@ -1,3 +1,8 @@
+// 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
+// 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+// 例如输入前序遍历序列 {1,2,4,7,3,5,6,8}
+// 和中序遍历序列 {4,7,2,1,5,3,8,6}，则重建二叉树并返回。
+
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -14,58 +19,91 @@ public:
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
-void printer(TreeNode* root) {
-    if (root == nullptr) return;
+void PreorderTranverse(TreeNode* root)
+{
+  if (root == nullptr) return;
+  cout << root->val << " ";
+  PreorderTranverse(root->left);
+  PreorderTranverse(root->right);
+}
+
+void InorderTranverse(TreeNode* root)
+{
+  if (root == nullptr) return;
+  InorderTranverse(root->left);
+  cout << root->val << " ";
+  InorderTranverse(root->right);
+}
+
+class Solution
+{
+public:
+  TreeNode* reConstructBinaryTree(vector<int> pre, vector<int> in)
+  {
+    if (pre.size() != in.size()) return nullptr;
+    if (pre.size() < 1) return nullptr;
+
+    preorder_ = pre;
+    inorder_ = in;
     
-    queue<TreeNode*> todo;
-    todo.push(root);
-    while (!todo.empty()) {
-        TreeNode* p = todo.front();
-        if (p -> left)
-            todo.push(p -> left);
-        if (p -> right)
-            todo.push(p -> right);
-        cout << p -> val << ' ';
+    return build(0, preorder_.size(), 0, inorder_.size());
+  }
+
+private:
+  /**
+   * - pb: preorder begin index
+   * - pe: preorder end index (exclude)
+   * - ib: inorder begin index
+   * - ie: inorder end index (exclude)
+   */
+  TreeNode* build(int pb, int pe, int ib, int ie)
+  {
+    if (pe - pb < 1)
+      return nullptr;
+    // pe - pb >= 1 o.w.
+
+    TreeNode* root = new TreeNode(preorder_[pb]); 
+    
+    int pos = find_in(ib, ie, preorder_[pb]);
+    // compute left-part
+    int lpb = pb + 1;
+    int lpe = lpb + (pos - ib);
+    int lib = ib;
+    int lie = pos;
+    // compute right-part
+    int rpb = lpe;
+    int rpe = pe;
+    int rib = pos + 1;
+    int rie = ie;
+    // recursive
+    root->left = build(lpb, lpe, lib, lie);
+    root->right = build(rpb, rpe, rib, rie);
+
+    return root;
+  }
+
+  int find_in(int ib, int ie, int target)
+  {
+    int ret = ib;
+    for (; ret != ie; ++ret)
+    {
+      if (inorder_[ret] == target)
+        break;
     }
-}
+    return ret;
+  }
 
-TreeNode* build(vector<int>::iterator s1,
-        vector<int>::iterator t1,
-        vector<int>::iterator s2,
-        vector<int>::iterator t2
-        ) {
-    // boundary case
-    if (s2 >= t2) return nullptr;
-
-    TreeNode a(*s1);
-    TreeNode* root = &a;
-    auto pos = std::find(s2, t2, *s1);
-    int len1 = pos - s2;
-    int len2 = t2 - (pos + 1);
-    root -> left = build(s1+1,
-            s1+1+len1,
-            s2,
-            s2+len1);
-    root -> right = build(t1-len1,
-            t1,
-            t2-len2,
-            t2);
-    return root;
-}
-
-TreeNode* reconstruct(vector<int> pre, vector<int> in) {
-    vector<int>::iterator s1 = pre.begin(),
-        t1 = pre.end(),
-        s2 = in.begin(),
-        t2 = in.end();
-    TreeNode* root = build(s1, t1, s2, t2);
-    return root;
-}
+private:
+  vector<int> preorder_;
+  vector<int> inorder_;
+};
 
 int main() {
     vector<int> pre = {1,2,4,7,3,5,6,8};
     vector<int> in = {4,7,2,1,5,3,8,6};
-    TreeNode* root = reconstruct(pre, in);
-    printer(root);
+    TreeNode* root = Solution().reConstructBinaryTree(pre, in);
+    PreorderTranverse(root);
+    cout << "\n";
+    InorderTranverse(root);
     return 0;
 }
